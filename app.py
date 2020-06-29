23151fe9da44bf684810429dc12bd9a0645b2f29
Athreya Patel <athreyapatel@gmail.com>
2020-06-29 19:23:12 +0000

    Flask route with view

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask import Flask
 # Create Flask Object
 app = Flask(__name__)
 
+# Assign a URL route
+# Tell flask when to call view
+@app.route("/")
+def hello():
+    return "Hello"
+
 # Run app in main
 if __name__ == "__main__":
-    app.run()
-
-
+    app.run(debug=True, host = "0.0.0.0", port = 3000)
